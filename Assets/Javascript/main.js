e694d6a6b59fd1131870a04484e467e0f8364150
antoine <chabaud6@hotmail.com>
2018-12-10 14:39:40 +0100

    gestionnaire

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -5,3 +5,6 @@
  */
 let title = document.getElementById('main-title');
 title.innerHTML = 'Hello Javascript';
+
+// Créer une instance de Login, login est l'objet construit à partir de la classe Login.
+const login = new Login();
---
+++ SRC/User/login.class.js
@@ -0,0 +1,38 @@
+class Login {
+    constructor() {
+        //$ est le raccourcis jquery et document est le selecteur, attr (attribute) est la méthode (de jquery) et on définir l'attribue entre (). 
+        //dans l'idex c'est le mot entre les balises titre. le identifiez vous sera la nouvelle valeur du titre.
+        $(document).attr('title', 'Identifiez-vous');
+
+        //modif le titre de la page
+        $('#main-title').html('Identifiez-vous');
+
+        //Définition du listener sur le formulaire, le listener a été défini juste en dessous (ce qu'in fait) et est appelé ds le constructor pour être exécuté par défault.
+        this.formListener();
+    }
+    /**
+    * Gestionnaire d'événement sur le formulaire login
+    * @param void
+    * @return void
+    */
+    formListener() {
+        $('#loginForm').on(
+            'keyup',
+            //CallBack : intervient ssi l'événement défini survient (ici c'est un changement que l'on a defini par change)
+            function(event) {
+                //verif le contenu des champs
+                const login = $('[name="loginField"]');
+                const password = $('[name="passwordField"]');
+
+                // Est-ce que les 2 champs sont remplis le bouton est actif, sinon on lui précise de le désavtiver
+                if (login.val() !== '' && password.val() !== '') {
+                    $('#btnLogin').removeAttr('disabled');
+                } else {
+                    $('#btnLogin').attr('disabled', 'disabled');
+                }
+
+                console.log('Login : ' + login.val() + password.val());
+            }
+        );
+    }
+}
